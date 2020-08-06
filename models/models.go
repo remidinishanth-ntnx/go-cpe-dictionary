272f6621a47cfcd3d09a9709a01091f3d4ff9ee1
remidinishanth <nishanth.reddy@nutanix.com>
2020-08-06 19:56:02 +0530

    Add title to cpe strings

--- models/models.go
+++ models/models.go
@@ -7,18 +7,8 @@ import (
 // CategorizedCpe :
 // https://cpe.mitre.org/specification/CPE_2.3_for_ITSAC_Nov2011.pdf
 type CategorizedCpe struct {
-	gorm.Model      `json:"-" xml:"-"`
-	CpeURI          string
-	CpeFS           string
-	Part            string
-	Vendor          string
-	Product         string
-	Version         string
-	Update          string
-	Edition         string
-	Language        string
-	SoftwareEdition string
-	TargetSoftware  string
-	TargetHardware  string
-	Other           string
+	gorm.Model `json:"-" xml:"-"`
+	Title      string
+	CpeURI     string
+	CpeFS      string
 }
